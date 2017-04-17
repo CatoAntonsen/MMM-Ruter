e0b6378cc49ca74eeb99aef68c43f99449e44a87
Cato Antonsen <cato.antonsen@gmail.com>
2017-04-17 03:09:14 +0200

    Version 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 #MMM-Ruter Change Log
 
+## [1.1.0] - 2017-04-17
+
+- Now it's possible to add multiple instances of the module
+- Added some padding between line number and stop name
+- Set default animationSpeed to 0 to prevent "blinking" when module updates often
+- Moved all service calls and logic from node_helper.js ("backend") to MMM-Ruter.js ("frontent") to simplify a rather complex logic. My initial goal by putting everything in the backend, was to reduce number of calls to the service. That was not a good design decision...
+
+
 ## [1.0.2] - 2016-11-01
 
 - Added timeToThere config value to stops
--- MMM-Ruter.js
+++ MMM-Ruter.js
@@ -9,16 +9,16 @@ Module.register("MMM-Ruter",{
 
 	// Default module config.
 	defaults: {
-		timeFormat: 'LT',		// This is set automatically based on global config
-		showHeader: false, 		// Set this to true to show header above the journeys (default is false)
-		showPlatform: false,	// Set this to true to get the names of the platforms (default is false)
-		maxItems: 5,			// Number of journeys to display (default is 5)
-		humanizeTimeTreshold: 15, // If time to next journey is below this value, it will be displayed as "x minutes" instead of time (default is 15 minutes)
-		serviceReloadInterval: 30000, // Refresh rate in MS for how often we call Ruter's web service. NB! Don't set it too low! (default is 30 seconds)
-		timeReloadInterval: 1000, // Refresh rate how often we check if we need to update the time shown on the mirror (default is every second)
-		animationSpeed: 1000,	// How fast the animation changes when updating mirror (default is 1 second)
-		fade: true,				// Set this to true to fade list from light to dark. (default is true)
-		fadePoint: 0.25			// Start on 1/4th of the list. 
+		timeFormat: 'LT',				// This is set automatically based on global config
+		showHeader: false, 				// Set this to true to show header above the journeys (default is false)
+		showPlatform: false,			// Set this to true to get the names of the platforms (default is false)
+		maxItems: 5,					// Number of journeys to display (default is 5)
+		humanizeTimeTreshold: 15, 		// If time to next journey is below this value, it will be displayed as "x minutes" instead of time (default is 15 minutes)
+		serviceReloadInterval: 30000, 	// Refresh rate in MS for how often we call Ruter's web service. NB! Don't set it too low! (default is 30 seconds)
+		timeReloadInterval: 1000, 		// Refresh rate how often we check if we need to update the time shown on the mirror (default is every second)
+		animationSpeed: 0,				// How fast the animation changes when updating mirror (default is 0 second)
+		fade: true,						// Set this to true to fade list from light to dark. (default is true)
+		fadePoint: 0.25					// Start on 1/4th of the list. 
 	},
 
 	getStyles: function () {
@@ -39,18 +39,9 @@ Module.register("MMM-Ruter",{
 	start: function() {
 		console.log(this.translate("STARTINGMODULE") + ": " + this.name);
 
-		this.config.identifier = this.identifier;
-
 		this.journeys = [];
-		this.previousTime = [];
-
-		this.sendSocketNotification("CONFIG"+this.config.identifier, this.config);
-		
+		this.previousJourneys = [];
 		var self = this;
-		
-		setInterval(function() {
-			self.updateDomIfNeeded();
-		}, this.config.timeReloadInterval);
 
  		// Set locale and time format based on global config
 		moment.locale(config.language);
@@ -60,14 +51,16 @@ Module.register("MMM-Ruter",{
 				this.config.timeFormat = 'h:mm A';
 		}
 
-	},
+		// Just to an initial poll. Otherwise we have to wait for the serviceReloadInterval
+		self.startPolling(); 
 
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "RUTER_UPDATE") {
-			if (payload.target_identifier == this.identifier) {
-                this.journeys = payload.stops;
-            }
-		}
+		setInterval(function() {
+			self.startPolling();
+		}, this.config.serviceReloadInterval);
+		
+		setInterval(function() {
+			self.updateDomIfNeeded();
+		}, this.config.timeReloadInterval);
 	},
 	
 	getDom: function() {
@@ -82,7 +75,8 @@ Module.register("MMM-Ruter",{
 			
 			for(var i = 0; i < this.journeys.length; i++) {
 
-				var tr = this.getTableRow(this.journeys[i]);
+				var journey = this.journeys[i];
+				var tr = this.getTableRow(journey);
 				
 				// Create fade effect. <-- stolen from default "calendar" module
 				if (this.config.fade && this.config.fadePoint < 1) {
@@ -108,6 +102,118 @@ Module.register("MMM-Ruter",{
 		}
 
 		return wrapper;
+	},
+
+	startPolling: function() {
+		var self = this;
+
+		var promises = [];
+		for(var i=0; i < this.config.stops.length; i++) {
+			promises.push(new Promise((resolv) => {
+				this.getStopInfo(this.config.stops[i], function(err, result) {
+					resolv(result);
+				});
+			}));
+		}
+		
+		Promise.all(promises).then(function(promiseResults) {
+			if (promiseResults.length > 0) {
+				var allJourneys = [];
+				for(var i=0; i < promiseResults.length; i++) {
+					allJourneys = allJourneys.concat(promiseResults[i])
+				}
+				
+				allJourneys.sort(function(a,b) {
+					var dateA = new Date(a.time);
+					var dateB = new Date(b.time);
+					return dateA - dateB;
+				});
+
+				self.journeys = allJourneys.slice(0, self.config.maxItems);
+			}
+		});
+	},
+	
+	updateDomIfNeeded: function() {
+		var needUpdate = false;
+		
+		for(var i=0; i < this.journeys.length; i++)  {
+			var time = this.formatTime(this.journeys[i].time);	
+			if (this.previousJourneys[i] == undefined || this.previousJourneys[i].lineName != this.journeys[i].lineName || this.previousJourneys[i].time != time) {
+				needUpdate = true;
+				this.previousJourneys[i] = {};
+				this.previousJourneys[i].lineName = this.journeys[i].lineName;
+				this.previousJourneys[i].time = time;
+			}
+		}
+		
+		if (needUpdate) {
+			this.updateDom(this.config.animationSpeed);
+		}
+	},
+
+	getStopInfo: function(stopItem, callback) {
+		var HttpClient = function() {
+			this.get = function(requestUrl, requestCallback) {
+				var httpRequest = new XMLHttpRequest();
+				httpRequest.onreadystatechange = function() { 
+					if (httpRequest.readyState == 4 && httpRequest.status == 200)
+						requestCallback(httpRequest.responseText);
+				}
+
+				httpRequest.open( "GET", requestUrl, true );            
+				httpRequest.send( null );
+			}
+		}
+
+		var shouldAddPlatform = function(platform, platformFilter) {
+			if (platformFilter == null || platformFilter.length == 0) { return true; } // If we don't add any interesting platformFilter, then we asume we'll show all
+			for(var i=0; i < platformFilter.length; i++) {
+				if (platformFilter[i] === platform) { return true; }
+			}
+			
+			return false;
+		};
+
+		var dateParam = ""
+		if (stopItem.timeToThere) {
+			var min = stopItem.timeToThere;
+			var timeAhead = moment(moment.now()).add(min, "minute").format().substring(0, 16);
+			console.log("Looking for journeys " + min + " minutes ahead in time.");
+			dateParam = "?datetime=" + timeAhead;
+		} else {
+			console.log("Looking for current journeys");
+		}
+		
+		var url = "http://reisapi.ruter.no/StopVisit/GetDepartures/" + stopItem.stopId + dateParam;
+		
+		var client = new HttpClient();
+
+		client.get(url, function(response) {
+			var stops = JSON.parse(response);
+
+			var allStopItems = new Array();
+
+			for(var j = 0; j < stops.length; j++) {
+				var journey = stops[j].MonitoredVehicleJourney;
+				
+				if (shouldAddPlatform(journey.MonitoredCall.DeparturePlatformName, stopItem.platforms)) {
+					var numBlockParts = null;
+					if (journey.TrainBlockPart != null) {
+						numBlockParts = journey.TrainBlockPart.NumberOfBlockParts;
+					}
+					allStopItems.push({
+						stopId: stopItem.stopId,
+						lineName: journey.PublishedLineName,
+						destinationName: journey.DestinationName,
+						time: journey.MonitoredCall.ExpectedDepartureTime,
+						platform: journey.MonitoredCall.DeparturePlatformName
+					});
+				}
+			};
+
+			callback(null, allStopItems)		
+		});
 	},
 	
 	getTableHeaderRow: function() {
@@ -139,6 +245,7 @@ Module.register("MMM-Ruter",{
 	
 	getTableRow: function(journey) {
 		var tdLine = document.createElement("td");
+		tdLine.className = "line";
 		var txtLine = document.createTextNode(journey.lineName);
 		tdLine.appendChild(txtLine);
 		
@@ -163,22 +270,6 @@ Module.register("MMM-Ruter",{
 		tr.appendChild(tdTime);
 		
 		return tr;
-	},
-	
-	updateDomIfNeeded: function() {
-		var needUpdate = false;
-		
-		for(var i=0; i < this.journeys.length; i++)  {
-			var time = this.formatTime(this.journeys[i].time);	
-			if (this.previousTime[i] != time) {
-				needUpdate = true;
-				this.previousTime[i] = time;
-			}
-		}
-		
-		if (needUpdate) {
-			this.updateDom(this.config.animationSpeed);
-		}
 	},
 	
 	formatTime: function(t) {
